6bc87756d58f059b8063e2749d8e2f22f220d426
sushilv3 <sushilv3rma@gmail.com>
2021-05-15 06:23:33 +0530

    added use state hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Heading from './Heading';
 import Para from './Para';
 import List from './List';
 import Greeting from './Greeting';
 
-function App() {
+
+
+
+const App = ()=> {
+
+const state = useState();
+
+const [count, setCount] = useState(0);
+
+const IncNum = () =>{
+  setCount(count + 1);
+  //  console.log('clicked ', count++);
+};
+
   return (
    <>
    <Heading />
@@ -13,6 +26,9 @@ function App() {
    <List />
    <Greeting />
 
+   <h1> {count} </h1>
+
+   <button onClick={IncNum}> Click Me</button>
 
    
    </>
