ccda45977bea175e186b035418dcb2a18f26c276
sushilv3 <sushilv3rma@gmail.com>
2021-05-17 12:53:50 +0530

    added comment and batter way to use context consumer

--- src/contextapi/ComponentB.jsx
+++ src/contextapi/ComponentB.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ComponentC from './ComponentC'
+import { FirstName, LastName } from '../App';
 
 const ComponentB = ()=>{
-    
+
+    const fname = useContext(FirstName);
+    const lname = useContext(LastName);
+
+
 return(
 <>
+    <h1>inside of ComponentB.. my name is {fname} {lname}</h1>
     <ComponentC />
 </>
 );
